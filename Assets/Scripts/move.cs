2b6cdc7bd34a3e15df7720f100b09702aa338132
lysses <1161042996@qq.com>
2025-02-09 15:44:39 +0800

    init

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    float canvasWidth = 309.985f * 2;  // Canvas 的宽度
+    float canvasHeight = 1320f; // Canvas 的高度
+    public float jumpHeight = 150f;
+    float timeToReach = 0.25f;  // 跳跃所需时间
+    public float gravity = 0.0f;    // 重力加速度
+    Vector2 initialVelocity;
+    public Vector2 Velocity;
+    Vector2 position;
+    public float test = 1;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("PlayerController: Rigidbody2D component is missing!");
+        }
+    }
+
+    void Start()
+    {
+        //UpdateGravityDirection(); // 初始化重力方向
+        gravity = 8.0f * jumpHeight / Mathf.Pow(timeToReach,2.0f);
+        Debug.Log("gravity,,"+ gravity);
+        initialVelocity = new Vector2(1, 0);
+        //设置初始位置、速度和重力加速度
+        transform.position = new Vector2(-309.985f, -400f);
+        rb.velocity = new Vector2(0.0f, 0.0f);
+        Physics2D.gravity = new Vector2(0f, 0f);
+        position = new Vector2(transform.position.x, transform.position.y);
+    }
+    private void Update()
+    {
+
+        // 按下 G 键切换重力方向
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("jump????");
+            initialVelocity = CalculateInitialVelocity(canvasWidth, jumpHeight);
+
+            if (transform.position.x < 0f)
+            {
+                //向右跳
+                rb.velocity = new Vector2(initialVelocity.x, initialVelocity.y);
+            }
+            else
+            {
+                //向左跳
+                rb.velocity = new Vector2(-initialVelocity.x, initialVelocity.y);
+            }
+
+            Physics2D.gravity = new Vector2(0, -gravity);
+            Debug.Log("水平时间：" + (canvasWidth / initialVelocity.x) +
+                "   垂直时间:" + Mathf.Sqrt(2 * jumpHeight / gravity) * 2 +
+                ((canvasWidth / initialVelocity.x) == Mathf.Sqrt(2 * jumpHeight / gravity) * 2));
+        }
+        /*Debug.LogError("水平时间：" + canvasWidth / initialVelocity.x +"   垂直时间:" + jumpHeight / initialVelocity.y);*/
+
+        Velocity = rb.velocity;
+    }
+    void FixedUpdate()
+    {
+        // 按下 G 键切换重力方向
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("jump????");
+            /*            initialVelocity = CalculateInitialVelocity(canvasWidth, jumpHeight) ;
+
+                        if (transform.position.x < 0f)
+                        {
+                            //向右跳
+                            rb.velocity = new Vector2(initialVelocity.x, initialVelocity.y);
+                        }
+                        else
+                        {
+                            //向左跳
+                            rb.velocity = new Vector2(-initialVelocity.x, initialVelocity.y);
+                        }
+
+                        Physics2D.gravity = new Vector2(0, -gravity);
+                        Debug.Log("水平时间：" + (canvasWidth / initialVelocity.x) +
+                            "   垂直时间:" + Mathf.Sqrt(2* jumpHeight/gravity)*2 + 
+                            ((canvasWidth / initialVelocity.x) == Mathf.Sqrt(2 * jumpHeight / gravity) * 2));*/
+        }
+        /*Debug.LogError("水平时间：" + canvasWidth / initialVelocity.x +"   垂直时间:" + jumpHeight / initialVelocity.y);*/
+
+        Velocity = rb.velocity;
+
+    }
+
+    Vector2 CalculateInitialVelocity(float width, float height)
+    {
+        // 计算水平方向初速度
+        float vx = width / timeToReach;
+
+        // 计算垂直方向初速度
+        //float vy = gravity * timeToReach;
+        float vy = 0.5f * gravity * timeToReach * test;
+
+        // 返回初速度向量
+        return new Vector2(vx * Mathf.Sign(initialVelocity.x), vy);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log($"碰撞对象: {collision.gameObject.name}");
+        rb.velocity = new Vector2(0.0f, 0.0f);
+        Physics2D.gravity = new Vector2(0f, 0f);
+        Debug.Log(position - new Vector2(transform.position.x, transform.position.y));
+        position = new Vector2(transform.position.x, transform.position.y);
+    }
+
+}
---
+++ Assets/Scripts/wall.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class wall : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private BoxCollider2D boxCollider;
+    private float canvasHeight; // Canvas 高度
+    Transform child;
+    List<GameObject>  children;
+    // 移动速度 (单位：米/秒)
+    public Vector2 moveSpeed = new Vector2(0f, -100f); 
+
+    void Awake()
+    {
+        // 获取 RectTransform 和 BoxCollider2D 组件
+        rectTransform = GetComponent<RectTransform>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 将 BoxCollider2D 的尺寸与 RectTransform 的尺寸同步
+        Vector2 size = rectTransform.rect.size;
+        boxCollider.size = size;
+
+        // 将 BoxCollider2D 的位置与 RectTransform 的中心同步
+        Vector2 offset = rectTransform.rect.center;
+        boxCollider.offset = offset;
+        canvasHeight = gameObject.GetComponent<RectTransform>().rect.height;
+        child = transform.GetChild(0);
+        Image originalImage = child.GetComponent<Image>();
+        RectTransform originalRect = child.GetComponent<RectTransform>();
+        if (originalImage == null || originalRect == null)
+        {
+            Debug.LogError("第一个子物体缺少 Image 或 RectTransform 组件！");
+            return;
+        }
+        children = new List<GameObject>();
+        // 复制第一个子物体 7 次并设置高度和颜色
+        for (int i = 0; i < 7; i++)
+        {
+            // 复制子物体
+            GameObject newChild = Instantiate(child.gameObject, transform);
+            newChild.name = $"Child_{i + 1}";
+
+            // 设置 RectTransform 的高度
+            RectTransform newRect = newChild.GetComponent<RectTransform>();
+            newRect.sizeDelta = new Vector2(newRect.sizeDelta.x, canvasHeight * 0.2f);
+
+            // 设置子物体的位置（从下到上排列）
+            newRect.anchoredPosition = new Vector2(transform.GetComponent<RectTransform>().anchoredPosition.x, (i - 3)* canvasHeight * 0.2f);
+
+            // 设置 Image 的颜色
+            Image newImage = newChild.GetComponent<Image>();
+            newImage.color = Random.ColorHSV(); // 如果没有颜色数组，随机设置颜色
+            children.Add(newChild);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float chiledPosition = 0;
+        for (int i = 0; i < 7; i++)
+        {
+            children[i].GetComponent<RectTransform>().anchoredPosition += moveSpeed * Time.deltaTime;
+
+            chiledPosition = children[i].GetComponent<RectTransform>().anchoredPosition.y;
+            if (chiledPosition < (-3f * canvasHeight * 0.2f))
+            {
+                Vector2 anchoredPosition = new Vector2(children[i].GetComponent<RectTransform>().anchoredPosition.x, 3f * canvasHeight * 0.2f);
+                children[i].GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+            }
+
+        }
+    }
+
+}
